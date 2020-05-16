99867ccc264bf33cf05434d0833967a613532e40
Gil Balsiger <gil.balsiger@gmail.com>
2020-05-16 15:08:38 +0200

    bad smell: data class, refactor: type migration from Orders to ArrayList<Order>

--- src/main/java/ch/heig/gen/labo4/OrdersWriter.java
+++ src/main/java/ch/heig/gen/labo4/OrdersWriter.java
@@ -1,20 +1,22 @@
 package ch.heig.gen.labo4;
 
-public class OrdersWriter {
-    private Orders orders;
+import java.util.ArrayList;
 
-    public OrdersWriter(Orders orders) {
+public class OrdersWriter {
+    private final ArrayList<Order> orders;
+
+    public OrdersWriter(ArrayList<Order> orders) {
         this.orders = orders;
     }
 
     public String getContents() {
         StringBuilder sb = new StringBuilder("{\"orders\": [");
 
-        for (int i = 0; i < orders.getOrdersCount(); i++) {
-            sb.append(orders.getOrder(i).toJSON());
+        for (Order order : orders) {
+            sb.append(order.toJSON());
         }
 
-        if (orders.getOrdersCount() > 0) {
+        if (orders.size() > 0) {
             sb.delete(sb.length() - 2, sb.length());
         }
 
