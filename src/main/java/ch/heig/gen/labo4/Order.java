de4b5baefafb8fee01451d1d7ece755a7d0a3c6e
Gil Balsiger <gil.balsiger@gmail.com>
2020-05-16 15:00:55 +0200

    bad smell: mysterious method location, refactor: move getOrderContents to Order class and rename to toJSON, update its return type

--- src/main/java/ch/heig/gen/labo4/Order.java
+++ src/main/java/ch/heig/gen/labo4/Order.java
@@ -26,4 +26,26 @@ public class Order {
     public void AddProduct(Product product) {
         products.add(product);
     }
+
+    public String toJSON() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        sb.append("\"id\": ");
+        sb.append(getOrderId());
+        sb.append(", ");
+        sb.append("\"products\": [");
+
+        for (int j = 0; j < getProductsCount(); j++) {
+            sb.append(getProduct(j).toJSON());
+        }
+
+        if (getProductsCount() > 0) {
+            sb.delete(sb.length() - 2, sb.length());
+        }
+
+        sb.append("]");
+        sb.append("}, ");
+
+        return sb.toString();
+    }
 }
--- src/main/java/ch/heig/gen/labo4/OrdersWriter.java
+++ src/main/java/ch/heig/gen/labo4/OrdersWriter.java
@@ -11,7 +11,7 @@ public class OrdersWriter {
         StringBuffer sb = new StringBuffer("{\"orders\": [");
 
         for (int i = 0; i < orders.getOrdersCount(); i++) {
-            getOrderContent(sb, orders.getOrder(i));
+            sb.append(orders.getOrder(i).toJSON());
         }
 
         if (orders.getOrdersCount() > 0) {
@@ -19,25 +19,6 @@ public class OrdersWriter {
         }
 
         return sb.append("]}").toString();
-    }
-
-    private void getOrderContent(StringBuffer sb, Order order) {
-        sb.append("{");
-        sb.append("\"id\": ");
-        sb.append(order.getOrderId());
-        sb.append(", ");
-        sb.append("\"products\": [");
-
-        for (int j = 0; j < order.getProductsCount(); j++) {
-            sb.append(order.getProduct(j).toJSON());
-        }
-
-        if (order.getProductsCount() > 0) {
-            sb.delete(sb.length() - 2, sb.length());
-        }
-
-        sb.append("]");
-        sb.append("}, ");
     }
 
 }
